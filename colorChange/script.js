95af292ab3f27aacefdf0af01f1d49611aa5c509
Subhas2003 <subhasmunain@gmail.com>
2025-07-30 00:44:25 +0530

    add to github

---
+++ colorChange/script.js
@@ -0,0 +1,30 @@
+const buttons = document.querySelectorAll(".button");
+// console.log(buttons)
+const body = document.querySelector("body");
+
+
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener("click", function (e) {
+    console.log(e);
+    console.log(e.target);
+
+    if (e.target.id === "grey" ) {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "lightyellow";
+    
+    }
+    if (e.target.id === "white") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "black";
+    }
+    if (e.target.id === "blue") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "white";
+    }
+    if (e.target.id === "yellow") {
+      body.style.backgroundColor = e.target.id;
+      body.style.color = "black";
+    }
+  });
+});
