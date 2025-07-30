dc17c17f097c407d89157287e02c987d8c5537a1
Subhas2003 <subhasmunain@gmail.com>
2025-07-31 01:23:31 +0530

    Add this new file

---
+++ backGroundColorChange/script.js
@@ -0,0 +1,29 @@
+//generate a random color
+
+const randomColor = function () {
+  const hex = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * 16)];
+  }
+  return color;
+};
+let intervalId;
+const startchangecolcor = function () {
+    if(!intervalId){
+        intervalId=setInterval(changebgcolor, 1000);
+    }
+  function changebgcolor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+  
+};
+
+const stopchangecolcor = function () {
+    clearInterval(intervalId)
+    intervalId=null
+};
+
+document.querySelector("#start").addEventListener("click", startchangecolcor);
+
+document.querySelector("#stop").addEventListener("click", stopchangecolcor);
